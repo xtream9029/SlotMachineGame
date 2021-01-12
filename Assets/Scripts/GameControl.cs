0101bc4fc0ee340b65455f3b86f79506174b2229
xtream9029 <qocn9029@naver.com>
2021-01-12 10:32:53 +0900

    upload files

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class GameControl : MonoBehaviour
+{
+    public static event Action HandlePulled = delegate { };
+
+    [SerializeField]//private 변수를 인스펙터에서 접근 가능하게 해주는 기능
+    private Text prizeText;//점수표시
+
+    [SerializeField]
+    private Row[] rows;//슬롯 열형 배열
+
+    [SerializeField]
+    private Transform handle;//손잡이의 위치
+
+    private int prizeValue;//실제 점수값
+
+    private bool resultsChecked = false;
+
+    void Update()
+    {
+        //슬롯의 릴이 돌고 점수가 나오는 부분을 컨트롤 하는 부분
+
+        //슬롯의 모든릴이 멈춰져있을때
+        if(!rows[0].rowStopped || !rows[1].rowStopped || !rows[2].rowStopped)
+        {
+            prizeValue = 0;
+            prizeText.enabled = false;
+            resultsChecked = false;
+        }
+
+        //슬롯의 모든릴이 돌고 있고 결과 체크값이 참일때
+        if (rows[0].rowStopped && rows[1].rowStopped && rows[2].rowStopped && !resultsChecked)
+        {
+            CheckResults();
+            prizeText.enabled = true;
+            prizeText.text = "Prize:" + prizeValue;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (rows[0].rowStopped && rows[1].rowStopped && rows[2].rowStopped)
+        {
+            StartCoroutine("PullHandle");//핸들을 클릭했을때 손잡이가 움직이도록 
+        }
+    }
+
+    //실제로 손잡이가 회전하는 부분을 구현한 부분
+    private IEnumerator PullHandle()
+    {
+        for (int i = 0; i < 15; i++)
+        {
+            handle.Rotate(0f, 0f, i);
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        HandlePulled();
+
+        for(int i = 0; i < 15; i += 5)
+        {
+            handle.Rotate(0f, 0f, -i);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    private void CheckResults()
+    {//슬롯의 점수를 결정짓는 부분
+
+        if (rows[0].stoppedSlot == "Diamond" && rows[1].stoppedSlot == "Diamond" || rows[0].stoppedSlot == "Diamond" && rows[2].stoppedSlot == "Diamond" 
+            || rows[1].stoppedSlot == "Diamond" && rows[2].stoppedSlot == "Diamond")
+            prizeValue = 100;
+
+        else if (rows[0].stoppedSlot == "Diamond" && rows[1].stoppedSlot == "Diamond" && rows[2].stoppedSlot == "Diamond")
+            prizeValue = 200;
+
+        else if (rows[0].stoppedSlot == "Crown" && rows[1].stoppedSlot == "Crown" || rows[0].stoppedSlot == "Crown" && rows[2].stoppedSlot == "Crown"
+            || rows[1].stoppedSlot == "Crown" && rows[2].stoppedSlot == "Crown")
+            prizeValue = 300;
+
+        else if (rows[0].stoppedSlot == "Crown" && rows[1].stoppedSlot == "Crown" && rows[2].stoppedSlot == "Diamond")
+            prizeValue = 400;
+
+        else if (rows[0].stoppedSlot == "Watermelon" && rows[1].stoppedSlot == "Watermelon" || rows[0].stoppedSlot == "Watermelon" && rows[2].stoppedSlot == "Watermelon"
+            || rows[1].stoppedSlot == "Watermelon" && rows[2].stoppedSlot == "Watermelon")
+            prizeValue = 500;
+
+        else if (rows[0].stoppedSlot == "Watermelon" && rows[1].stoppedSlot == "Watermelon" && rows[2].stoppedSlot == "Watermelon")
+            prizeValue = 600;
+
+        else if (rows[0].stoppedSlot == "Bar" && rows[1].stoppedSlot == "Bar" || rows[0].stoppedSlot == "Bar" && rows[2].stoppedSlot == "Bar"
+           || rows[1].stoppedSlot == "Bar" && rows[2].stoppedSlot == "Bar")
+            prizeValue = 700;
+
+        else if (rows[0].stoppedSlot == "Bar" && rows[1].stoppedSlot == "Bar" && rows[2].stoppedSlot == "Bar")
+            prizeValue = 800;
+
+        else if (rows[0].stoppedSlot == "7" && rows[1].stoppedSlot == "7" || rows[0].stoppedSlot == "7" && rows[2].stoppedSlot == "7"
+           || rows[1].stoppedSlot == "7" && rows[2].stoppedSlot == "7")
+            prizeValue = 1000;
+
+        else if (rows[0].stoppedSlot == "7" && rows[1].stoppedSlot == "7" && rows[2].stoppedSlot == "7")
+            prizeValue = 1500;
+
+        else if (rows[0].stoppedSlot == "Cherry" && rows[1].stoppedSlot == "Cherry" || rows[0].stoppedSlot == "Cherry" && rows[2].stoppedSlot == "Cherry"
+        || rows[1].stoppedSlot == "Cherry" && rows[2].stoppedSlot == "Cherry")
+            prizeValue = 2000;
+
+        else if (rows[0].stoppedSlot == "Cherry" && rows[1].stoppedSlot == "Cherry" && rows[2].stoppedSlot == "Cherry")
+            prizeValue = 3000;
+
+        else if (rows[0].stoppedSlot == "Mango" && rows[1].stoppedSlot == "Mango" || rows[0].stoppedSlot == "Mango" && rows[2].stoppedSlot == "Mango"
+       || rows[1].stoppedSlot == "Mango" && rows[2].stoppedSlot == "Mango")
+            prizeValue = 4000;
+
+        else if (rows[0].stoppedSlot == "Mango" && rows[1].stoppedSlot == "Mango" && rows[2].stoppedSlot == "Mango")
+            prizeValue = 5000;
+
+        resultsChecked = true;//결과 체크값을 참으로 바꿔줌
+    }
+
+}
---
+++ Assets/Scripts/Row.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Row : MonoBehaviour
+{
+    private int randomValue;
+    private float timeInterval;
+
+    public bool rowStopped;
+    public string stoppedSlot;//결정된 슬롯
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rowStopped = true;
+        GameControl.HandlePulled += StartRotating;
+    }
+
+    private void StartRotating()
+    {
+        stoppedSlot = "";
+        StartCoroutine("Rotate");
+    }
+
+    private IEnumerator Rotate()//코루틴
+    {
+        rowStopped = false;//슬롯머신의 릴이 돌아야 하므로
+        timeInterval = 0.025f;
+
+        //릴 돌리는 부분을 구현한것
+        //각릴에서 첫번째 심볼과 마지막 심볼이 같아야 봣을때 도는것처럼 보여 질수 있음
+        for(int i = 0; i < 30; i++)
+        {
+            if (transform.position.y <= -1.75f)
+            {
+                transform.position = new Vector2(transform.position.x, 3.5f);
+            }
+
+            else transform.position = new Vector2(transform.position.x, transform.position.y  -0.025f);
+
+            yield return new WaitForSeconds(timeInterval);
+        }
+
+        randomValue = Random.Range(60, 100);
+
+        switch(randomValue % 3)
+        {
+            case 1:
+                randomValue += 2;
+                break;
+            case 2:
+                randomValue += 1;
+                break;
+        }
+
+        for(int i = 0; i < randomValue; i++)
+        {
+            if (transform.position.y <= -1.75f)
+                transform.position = new Vector2(transform.position.x, 3.5f);
+
+            else transform.position = new Vector2(transform.position.x, transform.position.y - 0.25f);
+            
+            //실제로 릴이 회전하는 속도를 결정짓는 부분
+            if (i > Mathf.RoundToInt(randomValue * 0.25f))
+                timeInterval = 0.05f;
+            if (i > Mathf.RoundToInt(randomValue * 0.25f))
+                timeInterval = 0.1f;
+            if (i > Mathf.RoundToInt(randomValue * 0.25f))
+                timeInterval = 0.15f;
+            if (i > Mathf.RoundToInt(randomValue * 0.25f))
+                timeInterval = 0.2f;
+
+            yield return new WaitForSeconds(timeInterval);
+        }
+
+        //y좌표에 따라 슬롯을 결정하는 부분
+        if (transform.position.y == -1.75f)
+        {
+            stoppedSlot = "Diamond";
+        }
+        else if (transform.position.y == -1.0f)
+        {
+            stoppedSlot = "Crown";
+        }
+        else if (transform.position.y == -0.25f)
+        {
+            stoppedSlot = "Watermelon";
+        }
+        else if (transform.position.y == 0.5f)
+        {
+            stoppedSlot = "Bar";
+        }
+        else if (transform.position.y == 1.25f)
+        {
+            stoppedSlot = "7";
+        }
+        else if (transform.position.y == 2.0f)
+        {
+            stoppedSlot = "Cherry";
+        }
+        else if (transform.position.y == 2.75f)
+        {
+            stoppedSlot = "Mango";
+        }
+        else if (transform.position.y == 3.5f)
+        {
+            stoppedSlot = "Diamond";
+        }
+
+        rowStopped = true;
+
+    }//Rotate
+
+    private void OnDestroy()
+    {
+        GameControl.HandlePulled -= StartRotating;
+    }//OnDestroy
+}
